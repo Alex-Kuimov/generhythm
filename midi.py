0cc23272fe9f34e48015f37c60f304340094bc7b
Alex Kuimov <spoot1986@gmail.com>
2023-08-10 16:19:38 +0300

    Code refactoring

--- midi.py
+++ midi.py
@@ -1,7 +1,11 @@
 import os
+from collections import Counter
+from mido import MidiFile, MidiTrack, Message, MetaMessage
+from datetime import datetime
 import mido
 from mido import MidiFile, MidiTrack, Message
 import pprint
+import random
 
 def get_midi_files(folder_path):
     file_paths = []
@@ -43,3 +47,46 @@ def create_dict_sequences(notes):
                 pattern = []
 
     return patterns_dict
+
+
+def save_to_midi(prediction_output, dict_sequences, style, time, deviation):
+    output_notes = []
+    for predicted_note in prediction_output:
+        notes_str = dict_sequences[predicted_note + deviation] if predicted_note + deviation in dict_sequences else \
+        dict_sequences[predicted_note]
+
+        notes = notes_str.split(',')
+
+        t = time
+        for note in notes:
+            velocity = random.randint(50, 60)
+            note_on = Message(type='note_on', note=int(note), velocity=velocity, time=t)
+            output_notes.append(note_on)
+            t = 0
+
+        t = time
+        for note in notes:
+            note_on = Message(type='note_on', note=int(note), velocity=0, time=t)
+            output_notes.append(note_on)
+            t = 0
+
+    # meta_tempo =  MetaMessage('set_tempo', tempo=857143, time=0),
+    meta_signature = MetaMessage('time_signature', numerator=4, denominator=4, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0),
+
+    mid = MidiFile()
+
+    # mid.tracks.append(meta_tempo)
+    mid.tracks.append(meta_signature)
+
+    mid.tracks.append(output_notes)
+
+    now = datetime.now()
+    current_date = now.strftime("%Y-%m-%d")
+    current_time = now.strftime("%H-%M-%S")
+
+    filename = f"out/{style}-{current_date}_{current_time}.mid"
+    mid.save(filename)
+
+    print("Файл сохранен:", filename)
+
+    return filename
