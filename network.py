b0391ba28b7e5acdc0cede96341768aa87396525
Alex Kuimov <spoot1986@gmail.com>
2023-08-17 13:00:01 +0300

    New ver 2.0

--- network.py
+++ network.py
@@ -6,7 +6,6 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 import pprint
-from midi import save_to_midi
 
 
 def create_network(input_dim, num_features):
@@ -24,24 +23,18 @@ def train(model, input_data, output_data, epochs=20, batch_size=64, name='data')
     model.save('models/' + name + '.keras')
 
 
-def generate(model, x, dict_sequences, style, deviation, time, note_count):
+def predict(model, x):
     start = np.random.randint(0, len(x) - 1)
     pattern = x[start]
-
     prediction_output = []
 
-    for note_index in range(note_count):
+    for note_index in range(64):
         prediction_input = np.reshape(pattern, (1, len(pattern), 1))
         prediction = model.predict(prediction_input)
         predicted_note = np.argmax(prediction)
         prediction_output.append(predicted_note)
+
         pattern = np.append(pattern, predicted_note)
         pattern = pattern[1:len(pattern)]
 
-    counted_values = Counter(prediction_output)
-    unique_values = len(counted_values)
-
-    if unique_values < 3:
-        generate(model, x, dict_sequences, style, deviation, time, note_count)
-    else:
-        return save_to_midi(prediction_output, dict_sequences, style,  time, deviation)
+    return prediction_output
